599b0b9cd9963d4512d4defe4c330f8fe2bbc870
Enes <enesaygur387@gmail.com>
2026-06-29 01:49:18 +0300

    feat: implement logout functionality

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,8 +1,20 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
 import styles from "./Header.module.css";
 function Header() {
+  const navigate = useNavigate();
+  const { logout } = useAuth();
   return (
     <header className={styles.header}>
       <h3>Real World Dashboard</h3>
+      <button
+        onClick={() => {
+          logout();
+          navigate("/login");
+        }}
+      >
+        Logout
+      </button>
     </header>
   );
 }
--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,13 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/common/Sidebar";
 import styles from "./DashboardLayout.module.css";
+import Header from "../components/common/Header";
 
 function DashboardLayout() {
   return (
     <div className={styles.container}>
       <Sidebar />
       <div className={styles.content}>
-        <header>Header</header>
+        <Header />
         <main className={styles.main}>
           <Outlet />
         </main>
