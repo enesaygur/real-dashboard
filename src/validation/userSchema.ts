24879275ce18f4b29ad271b2829390ca450b685a
Enes <enesaygur387@gmail.com>
2026-07-09 17:59:33 +0300

    feat: add validation rules with Zod

--- src/validation/userSchema.ts
+++ src/validation/userSchema.ts
@@ -1,7 +1,14 @@
 import { z } from "zod";
 
 export const userSchema = z.object({
-  name: z.string(),
-  email: z.string(),
-  role: z.enum(["User", "Admin"]),
+  name: z
+    .string()
+    .min(3, "Name must be at least 3 characters.")
+    .max(50, "Name cannot exceed 50 characters."),
+  email: z.string().email("Please enter a valid email address."),
+  role: z.enum(["User", "Admin"], {
+    error: "Please select a valid role",
+  }),
 });
+
+export type UserFormData = z.infer<typeof userSchema>;
